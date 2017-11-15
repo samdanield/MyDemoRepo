9d0dc76fdb4052740354dbb3b7c74f2225b39106
Sam <lnc@DIN76000833.corp.capgemini.com>
2017-11-15 11:06:55 +0530

    Gethu da

--- Day2-MyApp/src/main/java/com/cap/boot/MainClass.java
+++ Day2-MyApp/src/main/java/com/cap/boot/MainClass.java
@@ -11,6 +11,8 @@ public class MainClass {
 
 	private static void greetings() {
 		System.out.println("Hello!");
+		System.out.println("Good Morning!");
+		System.out.println("Greetings to all!");
 	}
 
 }
